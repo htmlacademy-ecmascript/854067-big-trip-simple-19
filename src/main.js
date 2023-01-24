70c38bdc4fd8b235e31ab1f2fbd4806c73362fd9
inki-na-meeda <the_vlasov@mail.ru>
2023-01-24 19:51:14 +0300

    правки в задании Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import FilterView from './view/filter-view.js';
 import {render} from './render.js';
 import EventPresenter from './presenter/event-presenter.js';
 
-const eventPresenter = new EventPresenter({eventContainer: document.body});
 const tripEventsElement = document.querySelector('.trip-events');
+const eventPresenter = new EventPresenter({eventContainer: tripEventsElement});
 
 render(new HeaderView(), document.body, 'afterbegin');
 render(new FilterView(), tripEventsElement);
